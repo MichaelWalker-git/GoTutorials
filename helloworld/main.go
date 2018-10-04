7ee0df62bfa9f2c5d2d9ceb8edef015112e5fd6b
Michael Tran <michaelhuytran@gmail.com>
2018-10-03 21:15:07 -0700

    [Feature] - overview

--- helloworld/main.go
+++ helloworld/main.go
@@ -5,3 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hi there")
 }
+// package main- defines a package that can be compiled then executed, must have a fun called main
+//package calc- can be used as a dependency - helper code
+
+//package uploader - can be used as a dependency

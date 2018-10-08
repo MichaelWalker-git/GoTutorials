d628e34a845ab9c704804cd9024a7be1460c2d79
Michael Tran <michaelhuytran@gmail.com>
2018-10-07 19:49:54 -0700

    [Feature] - Add splitting of decks

--- cards/main.go
+++ cards/main.go
@@ -1,19 +1,10 @@
 package main
 
 func main(){
-	cards := cards.newDeck()
-	cards.print()
+	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
 }
-
-// Need to define our expected return value (Explicit typing)
-//func newCard() string {
-//	return "Five of Diamonds"
-//}
-
-// Variables can be declared outside of func(), however, they can't be assigned outside of func
-
-// Array - Fixed length list
-// Slice - An array of dynamic length
-
-// Both arrays and slices must be of identical types
-

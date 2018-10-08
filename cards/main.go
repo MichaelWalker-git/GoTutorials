63ae36e53a645a830f823bfda35449408c8f88fe
Michael Tran <michaelhuytran@gmail.com>
2018-10-07 19:08:52 -0700

    Abstract logic and add receiver function

--- cards/main.go
+++ cards/main.go
@@ -1,19 +1,14 @@
 package main
 
-import "fmt"
-
 func main(){
-	cards := []string {newCard(), newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Six of Spades")
-	for i, card:= range cards{
-		fmt.Println(i, card)
-	}
+	cards := cards.newDeck()
+	cards.print()
 }
 
 // Need to define our expected return value (Explicit typing)
-func newCard() string {
-	return "Five of Diamonds"
-}
+//func newCard() string {
+//	return "Five of Diamonds"
+//}
 
 // Variables can be declared outside of func(), however, they can't be assigned outside of func
 

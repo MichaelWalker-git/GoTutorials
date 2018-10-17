11ac288a6ba31ee03eb1910e51a80e8c9d3063a9
Michael Tran <michael.tran@walmartlabs.com>
2018-10-16 18:45:30 -0700

    [Feature] - Type conversion

--- cards/main.go
+++ cards/main.go
@@ -1,8 +1,13 @@
 package main
 
+import "fmt"
+
 func main(){
-	cards := cards.newDeck()
-	cards.print()
+	//cards := cards.newDeck()
+	//cards.print()
+
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
 
 // Need to define our expected return value (Explicit typing)
@@ -17,3 +22,11 @@ func main(){
 
 // Both arrays and slices must be of identical types
 
+
+// understanding func WriteFile(filename string, data []byte, perm os.FileMode) error
+//  data = raw data written to hard drive, slice of bytes (byte slice)
+// perm = permissions used to create that file, if we need to create it
+
+// a byte slice, think of a string, ascii character code
+
+//Type conversion

60e176c65b5cb585874c9b108f09226544bc1ac7
Michael Tran <michael.tran@walmartlabs.com>
2018-10-26 18:31:29 -0700

    [Feature] - Read from saved file

--- cards/deck.go
+++ cards/deck.go
@@ -3,50 +3,36 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
-	)
+)
 
-type deck[] string
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
 
-/*
- Receiver function -
- d - similar to reference "this" / "self"
- actual copy of deck (convention of GO, first letter of "deck")
- deck - call this function on itself
-*/
-
-/**
- * Creates a new deck of two different card arrays.
- */
 func newDeck() deck {
-	cards:= deck{}
+	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	//Replace unused variables with _
-	for _, shape:= range cardSuits {
-		for _, value:= range cardValues {
-			cards = append(cards, shape + " of " + value)
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
 		}
 	}
+
 	return cards
 }
 
-/**
- * Print receiver function on deck
- * Takes in a deck and prints iterative value over it
- */
 func (d deck) print() {
-	for i, card:= range d{
+	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
-/**
- * Takes one deck and returns two different decks based on a given handsize
- */
-func deal(d deck, handSize int) (deck, deck){
+func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
@@ -54,6 +40,20 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-func (d deck )saveFile(filename string) error {
+func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option 1 - Log the error and return a call to newDeck()
+		fmt.Println("ERROR: ", err)
+		os.Exit(1)
+		//return newDeck()
+	//	Option 2 - Log the error and quit the program
+
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,26 +1,6 @@
 package main
 
-func main(){
-	cards := newDeck()
-	cards.saveFile("my_cards")
+func main() {
+	cards := newDeckFromFile(":my_deck")
+	cards.print()
 }
-
-// Need to define our expected return value (Explicit typing)
-//func newCard() string {go
-//	return "Five of Diamonds"
-//}
-
-// Variables can be declared outside of func(), however, they can't be assigned outside of func
-
-// Array - Fixed length list
-// Slice - An array of dynamic length
-
-// Both arrays and slices must be of identical types
-
-// understanding func WriteFile(filename string, data []byte, perm os.FileMode) error
-//  data = raw data written to hard drive, slice of bytes (byte slice)
-// perm = permissions used to create that file, if we need to create it
-
-// a byte slice, think of a string, ascii character code
-
-// Type conversion
